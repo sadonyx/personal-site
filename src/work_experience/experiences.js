598e1a4e74cb41101f639a47a826844df3ace2ba
sadonyx <ashihabi12321@gmail.com>
2022-01-26 23:14:38 -0800

    correct grammar of exp

--- src/work_experience/experiences.js
+++ src/work_experience/experiences.js
@@ -3,25 +3,25 @@ export const experiences = [
     title: "KOXY Student Radio, Technical Manager",
     dates: "JAN 2018 - MAY 2020",
     description: [
-      "Oversee live-technical production for +400-person capacity musical events",
-      "Allocate funding and maintains inventory for technical equipment",
-      "Communicate with artist managers about necessary equipment, siting, and programming",
+      "Oversaw live-technical production for +400-person capacity musical events",
+      "Allocated funding and maintained inventory for technical equipment",
+      "Communicated with artist managers about necessary equipment, siting, and programming",
     ],
   },
   {
     title: "Occidental College Library Assistant",
     dates: "AUG 2016 - APR 2020",
     description: [
-      "Navigate students towards requested academic resources to achieve academic successes",
-      "Collaborate with other library departments to make a variety of academic resources more available",
+      "Guided students towards requested academic resources to achieve academic successes",
+      "Collaborated with other library departments to make a variety of academic resources more available",
     ],
   },
   {
     title: "Undergraduate Research, Occidental College",
     dates: "FEB 2019 - DEC 2019",
     description: [
-      "Analyze the effectiveness of physical activity and nutrition resources/interventions in changing lifestyle behaviors of low-income Angelinos",
-      "Collaborate with community partners to create educational material, plan events, conduct informative workshops, and evaluate intervention strategies",
+      "Analyzed the effectiveness of physical activity and nutrition resources/interventions in changing lifestyle behaviors of low-income Angelinos",
+      "Collaborated with community partners to create educational material, plan events, conduct informative workshops, and evaluate intervention strategies",
     ],
   },
 ];
